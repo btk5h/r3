f274498d9d815d82b6986c6fb18c78f69e4cbb67
Bryan Terce <bryan.terce@gmail.com>
2020-06-14 11:29:52 -0700

    Create labeled point component

---
+++ components/three/LabeledPoint.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ReactThreeFiber as R3F } from "react-three-fiber";
+import { Sphere } from "drei";
+import { Vector3 } from "three";
+import Label from "components/three/Label";
+
+type LabeledPointProps = {
+  position: Vector3;
+  color: R3F.Color;
+};
+
+const LabeledPoint: React.FC<LabeledPointProps> = (props) => {
+  const { position, color, children } = props;
+
+  return (
+    <Sphere position={position} args={[0.01]}>
+      <meshBasicMaterial attach="material" color={color} />
+      <Label anchorX={-0.01} anchorY="bottom" fontSize={0.05}>
+        {children}
+      </Label>
+    </Sphere>
+  );
+};
+
+export default LabeledPoint;
