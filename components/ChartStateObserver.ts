c3ec2017c6849a0013269f7e7b3ea998b4e38f9e
Bryan Terce <bryan.terce@gmail.com>
2020-06-16 20:05:59 -0700

    Debounce chart state to avoid excessive replaceState

--- components/ChartStateObserver.ts
+++ components/ChartStateObserver.ts
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useDebounce } from "use-debounce";
 import { ChartStoreHook } from "stores/chart";
 import { encodeChart } from "utils/chartEncoding";
 
@@ -9,14 +10,15 @@ type ChartStateObserverProps = {
 const ChartStateObserver: React.FC<ChartStateObserverProps> = (props) => {
   const { useChartStore } = props;
   const chartState = useChartStore((state) => encodeChart(state));
+  const [debouncedChartState] = useDebounce(chartState, 1000);
 
   useEffect(() => {
     window.history.replaceState(
       null,
       "",
-      `?c=${encodeURIComponent(chartState)}`
+      `?c=${encodeURIComponent(debouncedChartState)}`
     );
-  }, [chartState]);
+  }, [debouncedChartState]);
 
   return null;
 };
